fde5de9b18be138c0fa054587c5c4f6b1b6c3820
Steven Hartley <steven.c.hartley@gmail.com>
2020-11-06 20:28:26 +1300

    Adding large file chunking

--- Transcriber.py
+++ Transcriber.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
-from pprint import pprint
-from pydub import AudioSegment
-from os import path
 import os
 import ntpath
 import sys
 import json
 import speech_recognition as sr
+from pprint import pprint
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+from os import path
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
@@ -25,30 +26,50 @@ name, extension = os.path.splitext(sound_name)
 print('File extension:', extension )
 
 if extension.lower() == ".mp3":
-    print("is a mp3")
+    print("Changing MP3 to Wav.")
     temp_sound = AudioSegment.from_mp3(sound_location)
-    temp_sound.export("temp_mptowav.wav", format="wav")
-    sound_location = "temp_mptowav.wav"
+    temp_out = '{path}/temp/temp_mptowav.wav'.format(path=location)
+    temp_sound.export(temp_out, format="wav")
+    sound_location = temp_out
 
-harvard = sr.AudioFile(sound_location)
-with harvard as source:
-    audio = r.record(source)
+# Brake audio, into 40 second chunks.
+temp_sound = AudioSegment.from_file(sound_location, format="wav")
+chunks =  make_chunks(temp_sound,40000)
+print("Number of Chunks",len(chunks))
 
-# recognize speech using Google Cloud Speech
-g_cloud_json = open('{path}/credentials/googlecloud.json'.format(path=location))
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = json.dumps(json.load(g_cloud_json))
+# Export chunks
+for i, chunk in enumerate(chunks):
+    chunk_name = "chunk{0}.wav".format(i)
+    print ("exporting", chunk_name)
+    chunk.export("{path}/temp/{name}".format(path=location,name=chunk_name), format="wav")
 
-try:
-    print("Google Cloud Speech recognition results:")
-    out = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)  # pretty-print the recognition result
-    # out = r.recognize_google(audio)
-except sr.UnknownValueError:
-    print("Google Cloud Speech could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Cloud Speech service; {0}".format(e))
+all_text = []
 
-pprint(out)
+for i in range(len(chunks)):
+    harvard = sr.AudioFile("{path}/temp/chunk{number}.wav".format(path=location,number=i))
+    with harvard as source:
+        audio = r.record(source)
 
-file_out = open('{path}/{name}.rtf'.format(name=sound_name, path=out_location), 'w')
-file_out.write(out)
-file_out.close()
+    # recognize speech using Google Cloud Speech
+    g_cloud_json = open('{path}/credentials/googlecloud.json'.format(path=location))
+    GOOGLE_CLOUD_SPEECH_CREDENTIALS = json.dumps(json.load(g_cloud_json))
+
+    try:
+        print("Google Cloud Speech recognition results:")
+        out = r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)  # pretty-print the recognition result
+        # out = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Google Cloud Speech could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+
+    pprint(out)
+    all_text.append(out)
+
+transcript = ""
+for i, text in enumerate(all_text):
+    transcript = transcript + "{0}".format(text)
+
+with open('{path}/{name}.rtf'.format(name=sound_name, path=out_location), 'w') as file_out: 
+    file_out.write(transcript)
+
